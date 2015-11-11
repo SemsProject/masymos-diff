6eb3a6eccafd74e83031fee7df6ec8dcba246a89
mp487 <martin.peters3@uni-rostock.de>
2015-11-11 17:24:55 +0100

    moved main to CLI project

--- src/main/java/de/unirostock/sems/masymos/diff/DiffExecutor.java
+++ src/main/java/de/unirostock/sems/masymos/diff/DiffExecutor.java
@@ -17,7 +17,6 @@ import org.neo4j.graphdb.Transaction;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import de.unirostock.sems.masymos.configuration.Config;
 import de.unirostock.sems.masymos.database.Manager;
 import de.unirostock.sems.masymos.diff.configuration.NodeLabel;
 import de.unirostock.sems.masymos.diff.configuration.Property;
@@ -270,47 +269,4 @@ public class DiffExecutor {
 		}
 	}
 	
-	public static void main(String[] args) {
-		
-		boolean cleanDiffs = false;
-		long limit = 0;
-		
-		log.info("Started up");
-		
-		for (int i = 0; i < args.length; i++) {
-			if (args[i].equals("-dbPath")) {
-				Config.instance().setDbPath(args[++i]);
-			}
-			else if(args[i].equals("-cleanDiffs")) {
-				cleanDiffs = true;
-			}
-			else if(args[i].equals("-limit")) {
-				limit = Long.parseLong( args[++i] );
-			}
-		}
-		
-		// create neo4j database
-		long start = System.currentTimeMillis();
-		log.info("Getting manager...");
-		Manager.instance();
-		DiffExecutor executor = DiffExecutor.instance();
-		log.info("Got manager in {} mseconds", (System.currentTimeMillis() - start));
-		
-		if( cleanDiffs ) {
-			log.info("Removing existing diffs enabled");
-			start = System.currentTimeMillis();
-			executor.removeAllDiffs();
-			log.info("Cleaned up in {} mseconds", (System.currentTimeMillis() - start));
-		}
-		
-		log.info("start generating diffs");
-		if( limit > 0 )
-			log.info("Limit is set to {}", limit);
-		start = System.currentTimeMillis();
-		executor.execute(limit);
-		log.info("done in " + (System.currentTimeMillis() - start)
-				+ "ms");
-		
-		System.exit(0);
-	}
 }
